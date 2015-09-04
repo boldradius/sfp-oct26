1408462fc81483b397a2eb6cab5632c1071e1508
Mike Nash <michael.nash@weightwatchers.com>
2015-09-04 14:40:00 -0600

    Airports and codes

---
+++ src/main/scala/com/boldradius/domain/Airport.scala
@@ -0,0 +1,11 @@
+package com.boldradius.domain
+import scala.language.implicitConversions
+
+case class Airport(code: AirportCode, name: String)
+
+object AirportCode {
+  implicit def toCode(str: String) = AirportCode(str)
+}
+case class AirportCode(str: String) {
+  require(str.matches("[A-Z][A-Z][A-Z]"), "Must be 3 uppercase letters")
+}
--- src/test/scala/com/boldradius/domain/DomainTests.scala
+++ src/test/scala/com/boldradius/domain/DomainTests.scala
@@ -3,4 +3,28 @@ package com.boldradius.domain
 import org.scalatest.FreeSpec
 
 class DomainTests extends FreeSpec {
+  "the example domain" - {
+    "an airport" - {
+      "has a code and name" in {
+        val code = AirportCode("YXE")
+        val name = "Saskatoon"
+        val airport = Airport(code, name)
+        assert(airport.code === code)
+        assert(airport.name === name)
+      }
+    }
+    "an airport code" - {
+      "must be 3 uppercase letters" in {
+        intercept[IllegalArgumentException] {
+          AirportCode("foo")
+        }
+      }
+      "can be created from a string" in {
+        val yxe = "YXE"
+        def mkCode(code: AirportCode) = code
+        assert(mkCode(yxe) === AirportCode("YXE"))
+      }
+    }
+  }
+
 }
