3229eb0ee338422b96abc73e7e5d19c42833c228
Brendan McAdams <bwmcadams@evilmonkeylabs.com>
2015-09-10 15:19:52 -0700

    More idiomatic usage of sequence pattern matching.

--- src/main/scala/com/boldradius/training/boldAir/PowerOfRecursion.scala
+++ src/main/scala/com/boldradius/training/boldAir/PowerOfRecursion.scala
@@ -1,8 +1,8 @@
 package com.boldradius.training.boldAir
 
 object PowerOfRecursion {
-  def map[A,B](seq: Seq[A])(fn: A => B): Seq[B] = seq match {
-    case Seq() => Seq()
-    case Seq(first, rest @ _*) => fn(first) +: map(rest)(fn)
+  def map[A, B](seq: Seq[A])(f: A => B): Seq[B] = seq match {
+    case first +: rest => f(first) +: map(rest)(f)
+    case _ => Nil
   }
 }
