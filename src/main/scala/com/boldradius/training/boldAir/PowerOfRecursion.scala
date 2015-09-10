b29e3d3c436d2df852361f63e836365435074905
Andrew Mohrland <cxfreeio@gmail.com>
2015-09-10 11:50:04 -0700

    Power of Recursion: Answer - `map`

--- src/main/scala/com/boldradius/training/boldAir/PowerOfRecursion.scala
+++ src/main/scala/com/boldradius/training/boldAir/PowerOfRecursion.scala
@@ -1,5 +1,8 @@
 package com.boldradius.training.boldAir
 
 object PowerOfRecursion {
-  def map[A,B](seq: Seq[A])(fn: A => B): Seq[B] = ???
+  def map[A,B](seq: Seq[A])(fn: A => B): Seq[B] = seq match {
+    case Seq() => Seq()
+    case Seq(first, rest @ _*) => fn(first) +: map(rest)(fn)
+  }
 }
