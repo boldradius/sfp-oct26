d845a07b5861d3957fa1506cdf4b3d3ba2dcd2c6
Andrew Mohrland <cxfreeio@gmail.com>
2015-09-10 06:29:14 -0700

    Answer: Tail Recursion

--- src/main/scala/com/boldradius/training/boldAir/services/Reservations.scala
+++ src/main/scala/com/boldradius/training/boldAir/services/Reservations.scala
@@ -5,5 +5,12 @@ import com.github.nscala_time.time.Imports._
 import squants.market._
 
 object Reservations {
-  def totalPrice(itinerary: TentativeItinerary): Money = ???
+  def totalPrice(itinerary: TentativeItinerary): Money = {
+    @annotation.tailrec
+    def helper(flights: Seq[Flight], accum: Money): Money = flights match {
+      case Seq() => accum
+      case Seq(f, rest @ _*) => helper(rest, f.price + accum)
+    }
+    helper(itinerary.flights, USD(0))
+  }
 }
