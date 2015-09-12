84ee3b59253cf78427775cb844bfaa952ebbce4f
Andrew Mohrland <cxfreeio@gmail.com>
2015-09-12 15:14:05 -0700

    Remove `final`s

--- src/main/scala/com/boldradius/training/boldAir/domain/Airline.scala
+++ src/main/scala/com/boldradius/training/boldAir/domain/Airline.scala
@@ -1,9 +1,9 @@
 package com.boldradius.training.boldAir.domain
 
 sealed trait AircraftType
-final case object MD11 extends AircraftType
-final case object `747` extends AircraftType
-final case object Ospray extends AircraftType
+case object MD11 extends AircraftType
+case object `747` extends AircraftType
+case object Ospray extends AircraftType
 
 case class Aircraft(aircraftType: AircraftType, id: String)
 case class Airline(name: String, aircraft: Set[Aircraft])
@@ -14,14 +14,14 @@ case class Seat(row: Int, position: String)
  * Perfect core for future exercise regarding Abstract Types...
  * Passenger would have an abstract type Nationality, which would
  * need to be filled in on a concrete instance of Passenger
- * 
+ *
  * Also, add "FrequentFlyer" info... maybe instead?
  */
 sealed trait Passenger
-final case class Canadian(name: String) extends Passenger
-final case class American(name: String) extends Passenger
+case class Canadian(name: String) extends Passenger
+case class American(name: String) extends Passenger
 
 
 sealed trait LandingSurface
-final case object LandingStrip extends LandingSurface
-final case object LandingPad extends LandingSurface
+case object LandingStrip extends LandingSurface
+case object LandingPad extends LandingSurface
