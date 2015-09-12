0143d8e4582732ff6419159b7fa029422e163f6b
Brendan McAdams <bwmcadams@evilmonkeylabs.com>
2015-09-12 14:42:33 -0700

    Notes on future factoring for exercise

--- src/main/scala/com/boldradius/training/boldAir/domain/Airline.scala
+++ src/main/scala/com/boldradius/training/boldAir/domain/Airline.scala
@@ -10,6 +10,13 @@ case class Airline(name: String, aircraft: Set[Aircraft])
 case class Seat(row: Int, position: String)
 
 
+/**
+ * Perfect core for future exercise regarding Abstract Types...
+ * Passenger would have an abstract type Nationality, which would
+ * need to be filled in on a concrete instance of Passenger
+ * 
+ * Also, add "FrequentFlyer" info... maybe instead?
+ */
 sealed trait Passenger
 final case class Canadian(name: String) extends Passenger
 final case class American(name: String) extends Passenger
