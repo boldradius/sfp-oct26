b69632b5bea485f45192886453c4d823ec2a12a0
Brendan McAdams <brendan@boldradius.com>
2015-09-12 17:02:31 -0700

    Clean up Specs:     - mark TODO on missing items     - Rename all tests to "Spec" postfix in file and className

---
+++ src/test/scala/training/scala/air_scala/AkkaStreamsSpec.scala
@@ -0,0 +1,8 @@
+package training.scala.air_scala
+
+import org.scalatest.FreeSpec
+import org.scalatest.MustMatchers
+
+class AkkaStreamsSpec extends FreeSpec with MustMatchers {
+// TODO: Use Akka Streams to receive near real-time updates of position data on planes
+}
---
+++ src/test/scala/training/scala/air_scala/CanBuildFromSpec.scala
@@ -0,0 +1,8 @@
+package training.scala.air_scala
+
+import org.scalatest.FreeSpec
+import org.scalatest.MustMatchers
+
+class CanBuildFromSpec extends FreeSpec with MustMatchers {
+  // TODO: `CanBuildFrom`
+}
