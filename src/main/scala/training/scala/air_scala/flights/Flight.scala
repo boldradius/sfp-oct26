b91a6daaef2c69fe024ac26665daf310dc22045d
Sheena Luu <sheena.luu@boldradius.com>
2015-09-21 12:21:18 -0400

    make Flight not a case class and implement apply, unapply

--- src/main/scala/training/scala/air_scala/flights/Flight.scala
+++ src/main/scala/training/scala/air_scala/flights/Flight.scala
@@ -4,4 +4,9 @@ import squants.market.Money
 import training.scala.air_scala.aircraft.Aircraft
 import training.scala.air_scala.flights.scheduling._
 
-case class Flight(aircraft: Aircraft, schedule: Schedule, price: Money)
+class Flight(val aircraft: Aircraft, val schedule: Schedule, val price: Money)
+
+object Flight {
+  def apply(aircraft: Aircraft, schedule: Schedule, price: Money): Flight =  new Flight(aircraft, schedule, price)
+  def unapply(flight: Flight): Option[(Aircraft, Schedule, Money)] = Some(flight.aircraft, flight.schedule, flight.price)
+}
