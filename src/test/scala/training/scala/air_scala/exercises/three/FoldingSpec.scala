30c371d13e2c819b4cc749b8207bc5e049aeea9f
Brendan McAdams <brendan@boldradius.com>
2015-09-25 16:10:27 -0700

    Full flightTime check test

--- src/test/scala/training/scala/air_scala/exercises/three/FoldingSpec.scala
+++ src/test/scala/training/scala/air_scala/exercises/three/FoldingSpec.scala
@@ -18,16 +18,18 @@ class FoldingSpec extends FreeSpec with MustMatchers {
     }
     "single item itinerary" - {
       "is the proper duration" in {
-        val itinerary = SFToNewarkItinerary
         // todo - abstract me out into a non-hardcoded value for total flight times here
-        Itinerary.totalFlightTime(itinerary).normalizedStandard() mustBe new Period(5, 40, 0, 0)
-      }
-    }
-    "non-empty itinerary" - {
-      "a valid itinerary should give us a proper duration of flights, with layover time" - {
-        // todo - abstract me out into a non-hardcoded value for total flight times here
+        Itinerary.totalFlightTime(SFToNewarkItinerary).normalizedStandard() mustBe new Period(5, 40, 0, 0)
+        Itinerary.totalFlightTime(NewarkToLondonItinerary).normalizedStandard() mustBe new Period(7, 30, 0, 0)
         Itinerary.totalFlightTime(SFToLondonItinerary).normalizedStandard() mustBe new Period(13, 10, 0, 0)
+        Itinerary.totalFlightTime(LondonToNewarkItinerary).normalizedStandard() mustBe new Period(8, 5, 0, 0)
+        Itinerary.totalFlightTime(NewarkToSFItinerary).normalizedStandard() mustBe new Period(6, 12, 0, 0)
+        Itinerary.totalFlightTime(LondonToSFItinerary).normalizedStandard() mustBe new Period(14, 17, 0, 0)
+        Itinerary.totalFlightTime(SFToLondonRoundTripItinerary).normalizedStandard() mustBe {
+          new Period(3, 27, 0, 0).withDays(1)
+        }
       }
     }
+
   }
 }
