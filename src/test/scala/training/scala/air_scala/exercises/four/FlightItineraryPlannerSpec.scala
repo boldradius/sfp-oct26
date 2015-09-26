6bd388bede62e9614364a496bfca586b67307b83
Brendan McAdams <brendan@boldradius.com>
2015-09-26 05:03:07 -0700

    Test file for #4

---
+++ src/test/scala/training/scala/air_scala/exercises/four/FlightItineraryPlannerSpec.scala
@@ -0,0 +1,10 @@
+package training.scala.air_scala.exercises.four
+
+import org.scalatest.{FreeSpec, MustMatchers}
+import training.scala.air_scala.TestData
+
+class FlightItineraryPlannerSpec extends FreeSpec with MustMatchers {
+  import TestData._
+  import com.github.nscala_time.time.Imports._
+
+}
