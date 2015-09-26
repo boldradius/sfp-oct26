cdf5412d1a319b3b9fe1d9bc7129ffa8edea3445
Brendan McAdams <brendan@boldradius.com>
2015-09-26 16:19:36 -0700

    Refactored Exercise to use FlightNumber + RegEx extraction

--- src/test/scala/training/scala/air_scala/exercises/five/CustomExtractorSpec.scala
+++ src/test/scala/training/scala/air_scala/exercises/five/CustomExtractorSpec.scala
@@ -1,5 +1,6 @@
 package training.scala.air_scala.exercises.five
 
+import org.scalatest.exceptions.TestFailedException
 import org.scalatest.{FreeSpec, MustMatchers}
 import squants.space.NauticalMiles
 import training.scala.air_scala.TestData
@@ -7,37 +8,46 @@ import training.scala.air_scala.aircraft.Aircraft
 import training.scala.air_scala.flights.{FlightNumber, Flight}
 
 class CustomExtractorSpec extends FreeSpec with MustMatchers {
+  import org.scalatest.Assertions._
   import TestData._
   import squants.market._
 
 
-  "The `apply` factory method for Flight" - {
-    "Will safely construct a Flight instance" - {
-      val flight = Flight(
-        FlightNumber("UA", 1683),
-        Aircraft(B747),
-        SFToNewarkSchedule,
-        USD(256.15),
-        NauticalMiles(2565)
-      )
+  "The `unapply` extractor method for FlightNumber" - {
+    "Will safely extract the contents of a String into a FlightNumber case class" in {
 
-      flight.number mustBe FlightNumber("UA", 1683)
-      flight.aircraft mustBe Aircraft(B747)
-      flight.schedule mustBe SFToNewarkSchedule
-      flight.price mustBe USD(256.15)
-      flight.miles mustBe NauticalMiles(2565)
-    }
-  }
-
-  "The `unapply` extractor method for Flight" - {
-    "Will safely extract the contents of a Flight class" - {
-      val f = EWRToLHRFlight
-      val extracted = f match {
-        case Flight(f.number, f.aircraft, f.schedule, f.price, f.miles) => true
-        case _ => false
+      "UA940" match {
+        case FlightNumber(code, number) =>
+          assert(code == "UA")
+          assert(number == 940)
+        case _ =>
+          fail("Invalid extraction of FlightNumber.")
       }
 
-      extracted mustBe true
+      "BA1023" match {
+        case FlightNumber(code, number) =>
+          assert(code == "BA")
+          assert(number == 1023)
+        case _ =>
+          fail("Invalid extraction of FlightNumber.")
+      }
+
+      "AA8999" match {
+        case FlightNumber(code, number) =>
+          assert(code == "AA")
+          assert(number == 8999)
+        case _ =>
+          fail("Invalid extraction of FlightNumber.")
+      }
+
+    }
+    "Will safely fail when input is bad" in {
+      "XYZNO123" match {
+        case FlightNumber(code, number) =>
+          fail("Invalid FlightNumber extraction.")
+        case _ =>
+          assert(true)
+      }
     }
   }
 
