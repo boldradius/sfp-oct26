9e4b20776b4636ad2d0c3b71f01e9f1c741cb6a4
Brendan McAdams <brendan@boldradius.com>
2015-09-26 15:43:04 -0700

    Clean up and re-case-class Flight. We are gonna use something else for extractor demos.

--- src/main/scala/training/scala/air_scala/flights/Flight.scala
+++ src/main/scala/training/scala/air_scala/flights/Flight.scala
@@ -1,31 +1,17 @@
 package training.scala.air_scala.flights
 
+import com.github.nscala_time.time.Imports._
 import org.joda.time.DateTime
 import squants.market.Money
-import training.scala.air_scala.aircraft.Aircraft
-import training.scala.air_scala.airport.{AirportCode, Gate}
-import training.scala.air_scala.flights.scheduling._
-import com.github.nscala_time.time.Imports._
-import squants._
-import squants.market._
 import squants.space._
+import training.scala.air_scala.aircraft.Aircraft
+import training.scala.air_scala.airport.AirportCode
 
-object Flight {
-  def apply(number: FlightNumber, aircraft: Aircraft, schedule: Schedule,
-             price: Money, miles: Length): Flight =
-    Flight(number, aircraft, schedule, price, miles)
-
-  def unapply(flight: Flight): Option[(FlightNumber, Aircraft, Schedule, Money, Length)] =
-    Option((flight.number, flight.aircraft, flight.schedule, flight.price, flight.miles))
-
-}
-
-class Flight(val number: FlightNumber,
-             val aircraft: Aircraft,
-             val schedule: Schedule,
-             val price: Money,
-             val miles: Length) extends Ordered[Flight] {
-
+case class Flight(val number: FlightNumber,
+  val aircraft: Aircraft,
+  val schedule: Schedule,
+  val price: Money,
+  val miles: Length) extends Ordered[Flight] {
 
   override val toString = s"Flight { number: $number, aircraft: $aircraft, schedule: $schedule, " +
     s"price: $price, miles: $miles }"
@@ -38,19 +24,16 @@ class Flight(val number: FlightNumber,
    * This may seem odd at first, but subtracting two flights needs to give us the COMBINED duration
    * of those flights, as such we *ADD* their durations.
    *
-   * NOTE:  normalizedStandard() on Period prevents oddities like "19 hours, 70 minutes"
-   *
    */
-  def -(that: Flight): Period = new Period(that.schedule.origin.time, this.schedule.destination.time).normalizedStandard()
+  def -(that: Flight): Period = new Period(that.schedule.origin.time, this.schedule.destination.time)
 
 }
 
-
 case class FlightNumber(airlineCode: String, number: Int) {
   require(airlineCode.matches("[A-Z]{1,3}"),
-          "Airline Code must consist of 1-3 uppercase letters.")
+    "Airline Code must consist of 1-3 uppercase letters.")
   require(number > 0 && number < 8999,
-          "Flight Number be between 1 & 8999")
+    "Flight Number be between 1 & 8999")
 }
 
 case class FlightLeg(code: AirportCode, time: DateTime) extends Ordered[FlightLeg] {
